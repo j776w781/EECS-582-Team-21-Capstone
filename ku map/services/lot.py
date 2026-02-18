e514c05cce36b67196e737b2b6aefbd085ef66cd
Evans Tinashe Chigweshe <chigwesheevanstinashe@gmail.com>
2026-02-18 01:09:54 -0600

    Updated the Lot Class and the Lot service Class

--- ku map/services/lot.py
+++ ku map/services/lot.py
@@ -1,9 +1,19 @@
+
 class Lot:
     def __init__(self, ID, name, permType, coordinates, descript=""):
         self.id = ID
         self.name = name
-        self.minPermit = permType
+        self.permit_type = permType
         self.loc = coordinates
         self.color = "#808080"
         self.descript = descript
+
+
+    def json_dictionary(self):
+        return {"id": self.id,
+                "name": self.name, 
+                "permit type": self.permit_type, 
+                "position": self.loc,
+                "description": self.descript, 
+                "color": self.color}
 
--- ku map/services/lotservice.py
+++ ku map/services/lotservice.py
@@ -3,24 +3,29 @@ import json
 
 
 class LotService:
-    def __init__(self):
+    def __init__(self, data_path = "../data/lots.json"):
         self.lots = []
         self.startup()
 
     def startup(self):
+        # Loading lots from Json file at server start up. 
         file = open("../data/lots.json")
         lot_data = json.load(file)
         file.close()
 
         for lot in lot_data:
+        # Returns list of all lost instances 
             newLot = Lot(lot["id"], lot["name"], lot["type"], lot["position"], lot["restrictions"])
             self.lots.append(newLot)
 
     def get_all(self):
+        # Returns list of all Lot instances 
         return self.lots
     
     def get_lot(self, id):
+        # Returns a single Lot by ID. 
         for lot in self.lots:
             if lot.id == id:
-                return lot
+                return 
+        return None 
     
